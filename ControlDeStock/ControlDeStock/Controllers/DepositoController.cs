de485971fd650765be1bf73fc2213ea9d155497b
chrismedina13 <chrismedina13@hotmail.com>
2021-12-02 00:10:51 -0300

    Creación proyecto

---
+++ ControlDeStock/ControlDeStock/Controllers/DepositoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlDeStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepositoController : ControllerBase
+    {
+        [HttpGet("Leer")]
+        public string LecturaGet(int id) {
+
+            return "Hola";
+        }
+
+        [HttpGet("Buscar")]
+        public string BusquedaGet(int id)
+        {
+
+            return "Chau";
+        }
+    }
+}
